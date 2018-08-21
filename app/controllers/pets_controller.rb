f1decb5bbd7884aec93aa0d052d585d18fe7385a
Jonathan Hong <jonhong777@gmail.com>
2018-08-21 01:35:42 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -25,7 +25,7 @@ class PetsController < ApplicationController
   end
 
   patch '/pets/:id' do
-    @pet = Pet.find_by(params[:id])
+    @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(params["owner"])
